008a967770d1520047cdf8d50fc278134eb3be2c
danbaker <danbaker951@gmail.com>
2022-12-03 00:31:13 -0600

    Solved Day3 puzzle 2

--- Day3/RucksackReorganization/Program.cs
+++ Day3/RucksackReorganization/Program.cs
@@ -31,5 +31,20 @@ void Part1()
 
 void Part2()
 {
-
+    using (var stream = new StreamReader("input.txt"))
+    {
+        var prioritySum = 0;
+        while (!stream.EndOfStream)
+        {
+            var rucksackContents1 = stream.ReadLine();
+            var rucksackContents2 = stream.ReadLine();
+            var rucksackContents3 = stream.ReadLine();
+            if (rucksackContents1 != null && rucksackContents2 != null && rucksackContents3 != null)
+            {
+                var errorItem = Rucksack.FindGroupBadge(rucksackContents1, rucksackContents2, rucksackContents3);
+                prioritySum += Rucksack.PriorityValue(errorItem);
+            }
+        }
+        Console.WriteLine($"Part 2: {prioritySum}");
+    }
 }
--- Day3/RucksackReorganization/Rucksack.cs
+++ Day3/RucksackReorganization/Rucksack.cs
@@ -49,6 +49,41 @@ namespace RucksackReorganization
         }
 
         /// <summary>
+        /// Finds the item that is contained in all three contents and returns the value
+        /// </summary>
+        /// <param name="contents1">rucksack contents of first elf</param>
+        /// <param name="contents2">rucksack contents of second elf</param>
+        /// <param name="contents3">rucksack contents of third elf</param>
+        /// <returns>item value possessed by all three elves</returns>
+        public static char FindGroupBadge(string contents1, string contents2, string contents3)
+        {
+            HashSet<char> firstHash = new HashSet<char>();
+            HashSet<char> secondHash = new HashSet<char>();
+
+            // narrow down contents1 to its unique items
+            foreach(var i in contents1)
+            {
+                firstHash.Add(i);
+            }
+
+            // narrow down contents2 to the unique items which are also contained in contents1
+            foreach(var i in contents2)
+            {
+                if (firstHash.Contains(i))
+                    secondHash.Add(i);
+            }
+
+            // lastly, search contents 3 for the item which is also in secondHash
+            foreach(var i in contents3)
+            {
+                if (secondHash.Contains(i))
+                    return i;
+            }
+
+            return '\0';
+        }
+
+        /// <summary>
         /// Converts an item to its priority value.
         /// a - z = 1 - 26
         /// A - Z = 27 - 52
