adf887c613673b779aecb8a702f44e07755d4959
danbaker <danbaker951@gmail.com>
2022-12-06 19:51:17 -0600

    Solved day 6 puzzle 1

---
+++ Day6/TuningTrouble/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Part1();
+
+Part2();
+
+Console.ReadKey();
+
+void Part1()
+{
+    using (var stream = new StreamReader("input.txt"))
+    {
+        Console.WriteLine($"Part 1: {FindStartPacket(stream)}");
+    }
+}
+
+void Part2()
+{
+
+}
+
+int FindStartPacket(StreamReader input)
+{
+    var charactersProcessed = 0;
+    Queue<char> streamQueue = new Queue<char>();
+
+    while(!input.EndOfStream)
+    {
+        if (streamQueue.Count == 4)
+            break;
+
+        var character = (char)input.Read();
+        while (streamQueue.Contains(character))
+        {
+            streamQueue.Dequeue();
+        }
+
+        streamQueue.Enqueue(character);
+        charactersProcessed++;
+    }
+
+    return charactersProcessed;
+}
