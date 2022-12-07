e019f0ba349fd19b838768c917dc824ab12bc5b0
danbaker <danbaker951@gmail.com>
2022-12-06 19:58:45 -0600

    Solved day 6 puzzle 2

--- Day6/TuningTrouble/Program.cs
+++ Day6/TuningTrouble/Program.cs
@@ -9,23 +9,26 @@ void Part1()
 {
     using (var stream = new StreamReader("input.txt"))
     {
-        Console.WriteLine($"Part 1: {FindStartPacket(stream)}");
+        Console.WriteLine($"Part 1: {FindStartPacket(stream, 4)}");
     }
 }
 
 void Part2()
 {
-
+    using (var stream = new StreamReader("input.txt"))
+    {
+        Console.WriteLine($"Part 2: {FindStartPacket(stream, 14)}");
+    }
 }
 
-int FindStartPacket(StreamReader input)
+int FindStartPacket(StreamReader input, int messageSize)
 {
     var charactersProcessed = 0;
     Queue<char> streamQueue = new Queue<char>();
 
     while(!input.EndOfStream)
     {
-        if (streamQueue.Count == 4)
+        if (streamQueue.Count == messageSize)
             break;
 
         var character = (char)input.Read();
