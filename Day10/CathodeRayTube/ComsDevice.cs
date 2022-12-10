34a195f6f0977a84d470a630512929410ae6d755
danbaker <danbaker951@gmail.com>
2022-12-10 14:35:58 -0600

    Finished day 10 puzzle 1

---
+++ Day10/CathodeRayTube/ComsDevice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CathodeRayTube
+{
+    public class ComsDevice
+    {
+        private int _ClockCycle;
+        public int ClockCycle { get { return _ClockCycle; } }
+
+        private int XRegister;
+        private List<int> SignalStrengths;
+
+        private const int FirstSignalCycle = 20; // send the first signal at 20 cycles
+        private const int SignalPeriod = 40;     // poll every 40 cycles after the first signal
+        private const int NoopCycles = 1;
+        private const int AddxCycles = 2;
+
+        public ComsDevice()
+        {
+            _ClockCycle = 0;
+            XRegister = 1;
+            SignalStrengths = new List<int>();
+        }
+
+        public void AddX(int number)
+        {
+            for(int i = 0; i < AddxCycles; i++)
+            {
+                // increment 1 clock cycle and send a signal if needed
+                _ClockCycle++;
+                SendSignalIfNeeded();
+            }
+
+            // complese the add X instruction
+            XRegister += number;
+        }
+
+        public void Noop()
+        {
+            // increment 1 clock cycle and send a signal if needed
+            _ClockCycle++;
+            SendSignalIfNeeded();
+        }
+
+        public int SignalStrengthSum()
+        {
+            return SignalStrengths.Sum(x => x);
+        }
+
+        private void SendSignalIfNeeded()
+        {
+            if(ClockCycle == (FirstSignalCycle + (SignalPeriod * SignalStrengths.Count)))
+            {
+                SignalStrengths.Add(XRegister * ClockCycle);
+            }
+        }
+    }
+}
---
+++ Day10/CathodeRayTube/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using CathodeRayTube;
+
+Part1();
+
+Part2();
+
+Console.ReadKey();
+
+void Part1()
+{
+    var device = new ComsDevice();
+    using (var stream = new StreamReader("input.txt"))
+    {
+        while (!stream.EndOfStream)
+        {
+            var line = stream.ReadLine();
+
+            var parse = line!.Split(' ');
+
+            
+            switch (parse[0])
+            {
+                case "noop":
+                    device.Noop();
+                    break;
+                case "addx":
+                    if (parse.Length < 2)
+                        throw new Exception("addx needs a number parameter");
+                    if (!int.TryParse(parse[1], out int val))
+                        throw new Exception($"{parse[1]} is not a number");
+                    device.AddX(val);
+                    break;
+                default:
+                    throw new Exception($"{parse[0]} is not a valid instruction");
+            }
+
+            // stop after 220
+            if (device.ClockCycle >= 220)
+                break;
+        }
+
+        Console.WriteLine($"Part 1: {device.SignalStrengthSum()}");
+    }
+}
+
+void Part2()
+{
+
+}
