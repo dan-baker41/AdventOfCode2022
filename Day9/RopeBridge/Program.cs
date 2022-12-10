e3560d6148b8e908f9b48b90de45eee3cb5e7f86
danbaker <danbaker951@gmail.com>
2022-12-09 22:48:42 -0600

    solved day 9 part 1

---
+++ Day9/RopeBridge/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using RopeBridge;
+
+Part1();
+
+Part2();
+
+Console.ReadKey();
+
+void Part1()
+{
+    var rope = new RopeThingy();
+
+    using (var stream = new StreamReader("input.txt"))
+    {
+        while(!stream.EndOfStream)
+        {
+            var line = stream.ReadLine();
+
+            rope.Move(line!);
+        }
+    }
+
+    Console.WriteLine($"Part 1: {rope.PositionsVisitedByTail()}");
+}
+
+void Part2()
+{
+
+}
---
+++ Day9/RopeBridge/RopeThingy.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopeBridge
+{
+    public class RopeThingy
+    {
+        private const int X = 0, Y = 1;
+        private int[] headPosition;
+        private int[] tailPosition;
+
+        private HashSet<string> positionsVisited;
+
+        public RopeThingy()
+        {
+            headPosition = new int[2] { 0, 0 };
+            tailPosition = new int[2] { 0, 0 };
+            positionsVisited = new HashSet<string>();
+            positionsVisited.Add("0,0"); // tail visites 0,0 by default
+        }
+
+        public void Move(string movement)
+        {
+            // make sure the movement string is the correct length
+            if (movement.Length < 3)
+                throw new ArgumentException("Movement input value is invalid");
+
+            // check the movement value
+            switch(movement[0])
+            {
+                case 'R':
+                    MoveRight(movement.Substring(2, movement.Length - 2));
+                    break;
+                case 'L':
+                    MoveLeft(movement.Substring(2, movement.Length - 2));
+                    break;
+                case 'U':
+                    MoveUp(movement.Substring(2, movement.Length - 2));
+                    break;
+                case 'D':
+                    MoveDown(movement.Substring(2, movement.Length - 2));
+                    break;
+                default:
+                    throw new ArgumentException("Movement direction is invalid");
+            }
+        }
+
+        public int PositionsVisitedByTail()
+        {
+            return positionsVisited.Count;
+        }
+
+        private void MoveUp(string val)
+        {
+            if(!int.TryParse(val.ToString(), out int value))
+            {
+                throw new ArgumentException("Movement value is not a number");
+            }
+            else
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    // increment the y position of the head
+                    headPosition[Y]++;
+
+                    // if the tail is more than 1 y position below the head, move the tail directly behind the head
+                    if (tailPosition[Y] <= headPosition[Y] - 2)
+                    {
+                        tailPosition[Y] = headPosition[Y] - 1;
+                        tailPosition[X] = headPosition[X];
+
+                        // add this position to the hash set
+                        positionsVisited.Add($"{tailPosition[X]},{tailPosition[Y]}");
+                    }
+                }
+            }
+        }
+
+        private void MoveDown(string val)
+        {
+            if (!int.TryParse(val.ToString(), out int value))
+            {
+                throw new ArgumentException("Movement value is not a number");
+            }
+            else
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    // decrement the y position of the head
+                    headPosition[Y]--;
+
+                    // if the tail is more than 1 y position above the head, move the tail directly behind the head
+                    if (tailPosition[Y] >= headPosition[Y] + 2)
+                    {
+                        tailPosition[Y] = headPosition[Y] + 1;
+                        tailPosition[X] = headPosition[X];
+
+                        // add this position to the hash set
+                        positionsVisited.Add($"{tailPosition[X]},{tailPosition[Y]}");
+                    }
+                }
+            }
+        }
+
+        private void MoveLeft(string val)
+        {
+            if (!int.TryParse(val.ToString(), out int value))
+            {
+                throw new ArgumentException("Movement value is not a number");
+            }
+            else
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    // decrement the x position of the head
+                    headPosition[X]--;
+
+                    // if the tail is more than 1 y position above the head, move the tail directly behind the head
+                    if (tailPosition[X] >= headPosition[X] + 2)
+                    {
+                        tailPosition[Y] = headPosition[Y];
+                        tailPosition[X] = headPosition[X] + 1;
+
+                        // add this position to the hash set
+                        positionsVisited.Add($"{tailPosition[X]},{tailPosition[Y]}");
+                    }
+                }
+            }
+        }
+
+        private void MoveRight(string val)
+        {
+            if (!int.TryParse(val.ToString(), out int value))
+            {
+                throw new ArgumentException("Movement value is not a number");
+            }
+            else
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    // decrement the x position of the head
+                    headPosition[X]++;
+
+                    // if the tail is more than 1 y position above the head, move the tail directly behind the head
+                    if (tailPosition[X] <= headPosition[X] - 2)
+                    {
+                        tailPosition[Y] = headPosition[Y];
+                        tailPosition[X] = headPosition[X] - 1;
+
+                        // add this position to the hash set
+                        positionsVisited.Add($"{tailPosition[X]},{tailPosition[Y]}");
+                    }
+                }
+            }
+        }
+    }
+}
