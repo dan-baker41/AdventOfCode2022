80f5f9da7cdb3f6ee52fcaa10246740013854ec4
danbaker <danbaker951@gmail.com>
2022-12-11 22:16:40 -0600

    Solved day 11 both puzzles

---
+++ Day11/MonkeyIntheMiddle/Monkey.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyIntheMiddle
+{
+    public enum OperationType
+    {
+        Add = 1,
+        Multiply = 2,
+        Square = 3
+    }
+
+    public class Monkey
+    {
+        public int MonkeyId { get; set; }
+
+        private Queue<UInt64> Items;
+        public readonly OperationType OperationType;
+        private int OperationValue;
+        public int TestValue;
+        private int MonkeyIdOnSuccess;
+        private int MonkeyIdOnFail;
+
+        public int LowestCommonDenominator;
+
+        private int _TimesItemInspected;
+        public int TimesItemsInspected { get { return _TimesItemInspected; } }
+
+        public Monkey(string init)
+        {
+            Items = new Queue<UInt64>();
+            _TimesItemInspected = 0;
+            LowestCommonDenominator = 0;
+
+            var lines = init.Split('\n');
+            if (lines.Length != 7)
+                throw new ArgumentException("Invalid number of lines");
+            else
+            {
+                MonkeyId = lines[0][lines[0].Length - 2] - '0'; // convert ascii value to int
+
+                // parse the starting items in the second line
+                var items = lines[1].Split(' ');
+                // start at index 4
+                for(var i = 4; i < items.Length; i++)
+                {
+                    Items.Enqueue(UInt64.Parse(items[i].Replace(",", "")));
+                }
+
+                // parse the operation in the third line
+                var operation = lines[2].Split(' ');
+                if (operation[operation.Length - 2] == "*")
+                    OperationType = OperationType.Multiply;
+                else
+                    OperationType = OperationType.Add;
+
+                if (int.TryParse(operation[operation.Length - 1], out int opVal))
+                    OperationValue = opVal;
+                else
+                {
+                    OperationType = OperationType.Square;
+                    OperationValue = 0;
+                }
+
+                // parse the test in the fourth line
+                var test = lines[3].Split(' ');
+                TestValue = int.Parse(test[test.Length - 1]);
+
+                // find out which monkey ID on success with the fifth line
+                MonkeyIdOnSuccess = lines[4][lines[4].Length - 1] - '0';
+
+                // find out which monkey ID on fail with the sixth line
+                MonkeyIdOnFail = lines[5][lines[5].Length - 1] - '0';
+            }
+        }
+    
+        public bool HasItems()
+        {
+            return Items.Count > 0;
+        }
+
+        public void PerformOperation(bool StillWorried, out int MonkeyId, out UInt64 Item)
+        {
+            // first, calculate the worry level of the current item
+            var item = Items.Dequeue();
+            UInt64 worryValue = item;
+
+            switch(OperationType)
+            {
+                case OperationType.Add:
+                    worryValue = item + (UInt64)OperationValue;
+                    break;
+                case OperationType.Multiply:
+                    worryValue = item * (UInt64)OperationValue;
+                    break;
+                case OperationType.Square:
+                    worryValue = item * item;
+                    break;
+                default:
+                    throw new Exception("Invalid operation type");
+            }
+
+            if (worryValue < item)
+                Console.WriteLine("help");
+            // monkey inspects item
+            _TimesItemInspected++;
+
+            if (!StillWorried)
+            {
+                // worry value is divided by 3 after inspection
+                worryValue /= 3;
+            }
+            else
+            {
+                worryValue %= (UInt64)LowestCommonDenominator;
+            }
+
+            // test the worry value
+            if(worryValue % (UInt64)TestValue == 0)
+            {
+                MonkeyId = MonkeyIdOnSuccess;
+            }
+            else
+            {
+                MonkeyId = MonkeyIdOnFail;
+            }
+
+            Item = worryValue;
+        }
+
+        public void GiveItem(UInt64 item)
+        {
+            Items.Enqueue(item);
+        }
+    }
+}
---
+++ Day11/MonkeyIntheMiddle/Program.cs
@@ -0,0 +1,138 @@
+﻿using MonkeyIntheMiddle;
+
+Part1();
+
+Part2();
+
+Console.ReadKey();
+
+void Part1()
+{
+    List<Monkey> monkeys = new List<Monkey>();
+
+    using (var stream = new StreamReader("input.txt"))
+    {
+        while(!stream.EndOfStream)
+        {
+            // read 6 lines
+            var monkeyInit = "";
+            for(var i = 0; i < 6; i++)
+                monkeyInit += stream.ReadLine() + "\n";
+
+            // discard 1 line
+            stream.ReadLine();
+
+            // create the monkey and add it to the list
+            monkeys.Add(new Monkey(monkeyInit));
+        }
+
+        // perform 20 rounds
+        for(int i = 0; i < 20; i++)
+        {
+            // for each round, each monkey gets a turn
+            foreach(var monkey in monkeys)
+            {
+                while(monkey.HasItems())
+                {
+                    monkey.PerformOperation(false, out int id, out UInt64 item);
+
+                    // add this item to monkey specified by id
+                    monkeys.ElementAt(id).GiveItem(item);
+                }
+            }
+        }
+
+        // find the top two monkeys
+        PriorityQueue<int, int> mostInspections = new PriorityQueue<int, int>();
+        foreach (var monkey in monkeys)
+        {
+            mostInspections.Enqueue(monkey.TimesItemsInspected, -monkey.TimesItemsInspected);
+        }
+
+        var highest = mostInspections.Dequeue();
+        var second = mostInspections.Dequeue();
+
+        Console.WriteLine($"Part 1: {highest * second}");
+    }
+}
+
+void Part2()
+{
+    List<Monkey> monkeys = new List<Monkey>();
+
+    using (var stream = new StreamReader("input.txt"))
+    {
+        while (!stream.EndOfStream)
+        {
+            // read 6 lines
+            var monkeyInit = "";
+            for (var i = 0; i < 6; i++)
+                monkeyInit += stream.ReadLine() + "\n";
+
+            // discard 1 line
+            stream.ReadLine();
+
+            // create the monkey and add it to the list
+            monkeys.Add(new Monkey(monkeyInit));
+        }
+
+        // for each monkey, add references to the monkeys they pass to
+        var lcm = LeastCommonMultiple(monkeys[0].TestValue, monkeys[1].TestValue);
+        for(var i = 2; i < monkeys.Count; i++)
+        {
+            lcm = LeastCommonMultiple(lcm, monkeys[i].TestValue);
+        }
+
+        // perform 10000 rounds
+        for (int i = 0; i < 10000; i++)
+        {
+            // for each round, each monkey gets a turn
+            foreach (var monkey in monkeys)
+            {
+                monkey.LowestCommonDenominator = lcm;
+                while (monkey.HasItems())
+                {
+                    monkey.PerformOperation(true, out int id, out UInt64 item);
+
+                    // add this item to monkey specified by id
+                    monkeys.ElementAt(id).GiveItem(item);
+                }
+            }
+        }
+
+        // find the top two monkeys
+        PriorityQueue<int, int> mostInspections = new PriorityQueue<int, int>();
+        foreach (var monkey in monkeys)
+        {
+            mostInspections.Enqueue(monkey.TimesItemsInspected, -monkey.TimesItemsInspected);
+        }
+
+        var highest = mostInspections.Dequeue();
+        var second = mostInspections.Dequeue();
+
+        Console.WriteLine($"Part 2: {(long)highest * (long)second}");
+    }
+}
+
+int LeastCommonMultiple(int a, int b)
+{
+    int num1, num2;
+    if (a > b)
+    {
+        num1 = a; num2 = b;
+    }
+    else
+    {
+        num1 = b; num2 = a;
+    }
+
+    for (int i = 1; i < num2; i++)
+    {
+        int mult = num1 * i;
+        if (mult % num2 == 0)
+        {
+            return mult;
+        }
+    }
+    return num1 * num2;
+}
